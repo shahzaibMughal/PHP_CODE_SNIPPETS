8871a98723f68ec8a80735897e2f4aff4ca25a7c
Shahzaib Mughal <32070883+shahzaibMughal@users.noreply.github.com>
2018-12-23 01:14:27 +0500

    new functions are added

--- validation_functions.php
+++ validation_functions.php
@@ -30,4 +30,49 @@
      return preg_match($email_regex, $email) === 1;
    }
 
+
+// has_length_greater_than('abcd', 3)
+  // * validate string length
+  // * spaces count towards length
+  // * use trim() if spaces should not count
+  function has_length_greater_than($value, $min) {
+    $length = strlen($value);
+    return $length > $min;
+  }
+
+  // has_length_less_than('abcd', 5)
+  // * validate string length
+  // * spaces count towards length
+  // * use trim() if spaces should not count
+  function has_length_less_than($value, $max) {
+    $length = strlen($value);
+    return $length < $max;
+  }
+
+  // has_length_exactly('abcd', 4)
+  // * validate string length
+  // * spaces count towards length
+  // * use trim() if spaces should not count
+  function has_length_exactly($value, $exact) {
+    $length = strlen($value);
+    return $length == $exact;
+  }
+
+  // has_length('abcd', ['min' => 3, 'max' => 5])
+  // * validate string length
+  // * combines functions_greater_than, _less_than, _exactly
+  // * spaces count towards length
+  // * use trim() if spaces should not count
+  function has_length($value, $options) {
+    if(isset($options['min']) && !has_length_greater_than($value, $options['min'] - 1)) {
+      return false;
+    } elseif(isset($options['max']) && !has_length_less_than($value, $options['max'] + 1)) {
+      return false;
+    } elseif(isset($options['exact']) && !has_length_exactly($value, $options['exact'])) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
  ?>
